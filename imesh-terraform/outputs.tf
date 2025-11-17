f3006fa88d961fe706f408d2185860b7753a730e
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 23:34:48 +0530

    Update outputs.tf

--- imesh-terraform/outputs.tf
+++ imesh-terraform/outputs.tf
@@ -1,10 +1,10 @@
 output "application_urls" {
   description = "URLs to access the deployed applications"
   value = {
-    ui          = "https://app.${var.domain}"
-    auth        = "https://auth.${var.domain}"
-    services    = "https://services.${var.domain}"
-    graphql     = "https://services.${var.domain}/graphql"
+    ui          = module.gateways.app_ingress_url
+    auth        = module.gateways.keycloak_ingress_url
+    services    = module.gateways.services_ingress_url
+    graphql     = "${module.gateways.services_ingress_url}/graphql"
   }
 }
 
@@ -13,7 +13,7 @@ output "keycloak_admin_credentials" {
   value = {
     username = var.keycloak_admin.username
     password = var.keycloak_admin.password
-    url      = "https://auth.${var.domain}"
+    url      = module.gateways.keycloak_ingress_url
   }
   sensitive = true
 }
@@ -28,14 +28,13 @@ output "keycloak_client_secrets" {
 
 output "namespaces" {
   description = "Created Kubernetes namespaces"
-  value = [
-    kubernetes_namespace.v2_deps.metadata[0].name,
-    kubernetes_namespace.v2.metadata[0].name,
-    kubernetes_namespace.v2_agent.metadata[0].name,
-    kubernetes_namespace.cert_manager.metadata[0].name,
-    kubernetes_namespace.envoy_gateway_system.metadata[0].name,
-    kubernetes_namespace.istio_system.metadata[0].name
-  ]
+  value = {
+    v2_deps    = kubernetes_namespace.v2_deps.metadata[0].name
+    v2         = kubernetes_namespace.v2.metadata[0].name
+    v2_agent   = kubernetes_namespace.v2_agent.metadata[0].name
+    cert_manager = kubernetes_namespace.cert_manager.metadata[0].name
+    ingress_nginx = "ingress-nginx"
+  }
 }
 
 output "deployment_status" {
@@ -45,7 +44,12 @@ output "deployment_status" {
     dependencies = module.dependencies.mongodb_status
     keycloak     = module.keycloak.configuration_complete
     applications = module.applications.deployed_services
-    gateways     = module.gateways.envoy_gateway_status
+    gateways     = module.gateways.nginx_ingress_status
     agent        = module.agent.agent_status
   }
+}
+
+output "ingress_info" {
+  description = "Ingress controller information"
+  value = module.gateways.ingress_controller
 }
