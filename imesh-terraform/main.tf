54e36f1c344a4b75013b1477ace201be824ba726
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 14:24:03 +0530

    Update main.tf

--- imesh-terraform/main.tf
+++ imesh-terraform/main.tf
@@ -1,41 +1,132 @@
-# In the dependencies module call in main.tf:
+# Create namespaces
+resource "kubernetes_namespace" "v2_deps" {
+  metadata {
+    name = "v2-deps"
+  }
+}
+
+resource "kubernetes_namespace" "v2" {
+  metadata {
+    name = "v2"
+  }
+}
+
+resource "kubernetes_namespace" "v2_agent" {
+  metadata {
+    name = "v2-agent"
+  }
+}
+
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "kubernetes_namespace" "envoy_gateway_system" {
+  metadata {
+    name = "envoy-gateway-system"
+  }
+}
+
+resource "kubernetes_namespace" "istio_system" {
+  metadata {
+    name = "istio-system"
+  }
+}
+
+# Deploy all modules
+module "certificates" {
+  source = "./modules/certificates"
+  
+  namespaces = {
+    v2      = kubernetes_namespace.v2.metadata[0].name
+    v2_deps = kubernetes_namespace.v2_deps.metadata[0].name
+  }
+  
+  domain = var.domain
+  email  = "pulak.das@imesh.ai"
+}
+
 module "dependencies" {
   source = "./modules/dependencies"
+  
   namespace = kubernetes_namespace.v2_deps.metadata[0].name
+  keycloak_admin = var.keycloak_admin
+  
+  depends_on = [kubernetes_namespace.v2_deps]
 }
 
-# In the applications module call in main.tf:
-module "applications" {
-  source = "./modules/applications"
-  # ... existing variables ...
-  
-  dependencies = {
-    mongodb_url = module.dependencies.mongodb_url
-    redis_url   = module.dependencies.redis_url  
-    nats_url    = module.dependencies.nats_url
-  }
-  
-  keycloak_config = {
-    url    = module.dependencies.keycloak_url
-    realm  = var.keycloak_realm.name
-    graphql_client_secret = module.keycloak.graphql_backend_secret
-  }
-}
-
-# In the agent module call in main.tf:
-module "agent" {
-  source = "./modules/agent"
-  # ... existing variables ...
-  
-  dependencies = {
-    nats_url = module.dependencies.nats_url
-  }
-}
-
-# In the keycloak module call in main.tf:
 module "keycloak" {
   source = "./modules/keycloak"
-  # ... existing variables ...
   
-  depends_on = [module.dependencies]
+  namespace    = kubernetes_namespace.v2_deps.metadata[0].name
+  domain       = var.domain
+  admin_user   = var.keycloak_admin.username
+  admin_pass   = var.keycloak_admin.password
+  
+  depends_on = [
+    module.dependencies
+  ]
+}
+
+module "applications" {
+  source = "./modules/applications"
+  
+  namespace = kubernetes_namespace.v2.metadata[0].name
+  domain    = var.domain
+  docker_registry = var.docker_registry
+  
+  keycloak_config = {
+    url    = "http://keycloak.${kubernetes_namespace.v2_deps.metadata[0].name}.svc.cluster.local"
+    realm  = "master"
+    graphql_client_secret = module.keycloak.graphql_backend_secret
+  }
+  
+  dependencies = {
+    mongodb_url = "mongodb://mongodb.${kubernetes_namespace.v2_deps.metadata[0].name}.svc.cluster.local"
+    redis_url   = "redis-master.${kubernetes_namespace.v2_deps.metadata[0].name}.svc.cluster.local"
+    nats_url    = "nats://nats.${kubernetes_namespace.v2_deps.metadata[0].name}.svc.cluster.local:4222"
+  }
+  
+  depends_on = [
+    module.dependencies,
+    module.keycloak,
+    kubernetes_namespace.v2
+  ]
+}
+
+module "gateways" {
+  source = "./modules/gateways"
+  
+  namespaces = {
+    v2      = kubernetes_namespace.v2.metadata[0].name
+    v2_deps = kubernetes_namespace.v2_deps.metadata[0].name
+  }
+  
+  domain = var.domain
+  
+  depends_on = [
+    module.certificates,
+    kubernetes_namespace.v2,
+    kubernetes_namespace.v2_deps
+  ]
+}
+
+module "agent" {
+  source = "./modules/agent"
+  
+  namespace = kubernetes_namespace.v2_agent.metadata[0].name
+  docker_registry = var.docker_registry
+  cluster_name = var.cluster_name
+  agent_token = var.agent_token
+  
+  dependencies = {
+    nats_url = "nats://nats.${kubernetes_namespace.v2_deps.metadata[0].name}.svc.cluster.local"
+  }
+  
+  depends_on = [
+    module.dependencies,
+    kubernetes_namespace.v2_agent
+  ]
 }
