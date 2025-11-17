3383ccff4b61e53eac2b622d73e69cd101c986c8
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 14:27:04 +0530

    Update main.tf

--- imesh-terraform/modules/dependencies/main.tf
+++ imesh-terraform/modules/dependencies/main.tf
@@ -82,6 +82,135 @@ resource "helm_release" "nats" {
   }
 }
 
-resource "kubectl_manifest" "keycloak" {
-  yaml_body = file("${path.module}/../../manifests/keycloak.yaml")
+resource "kubernetes_stateful_set" "keycloak" {
+  metadata {
+    name      = "keycloak"
+    namespace = var.namespace
+    labels = {
+      app = "keycloak"
+    }
+  }
+
+  spec {
+    service_name = ""
+    replicas     = 1
+
+    selector {
+      match_labels = {
+        app = "keycloak"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "keycloak"
+        }
+      }
+
+      spec {
+        container {
+          name  = "keycloak"
+          image = "keycloak/keycloak:26.2.5"
+          args  = ["start-dev", "--log-console-color=true", "--log-level=DEBUG"]
+
+          port {
+            container_port = 8080
+            name           = "http"
+          }
+
+          env {
+            name  = "KC_HTTP_ENABLED"
+            value = "false"
+          }
+
+          env {
+            name  = "KEYCLOAK_ADMIN"
+            value = var.keycloak_admin.username
+          }
+
+          env {
+            name  = "KEYCLOAK_ADMIN_PASSWORD"
+            value = var.keycloak_admin.password
+          }
+
+          env {
+            name  = "KC_PROXY_HEADERS"
+            value = "xforwarded"
+          }
+
+          env {
+            name  = "KC_HOSTNAME"
+            value = "https://auth.dev.imesh.ai/"
+          }
+
+          env {
+            name  = "KC_HOSTNAME_BACKCHANNEL_DYNAMIC"
+            value = "true"
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/realms/master"
+              port = 8080
+            }
+            initial_delay_seconds = 30
+            period_seconds        = 10
+            timeout_seconds       = 1
+          }
+
+          volume_mount {
+            name       = "keycloak-persistent-storage"
+            mount_path = "/opt/keycloak/data"
+          }
+        }
+
+        volume {
+          name = "keycloak-persistent-storage"
+          persistent_volume_claim {
+            claim_name = "keycloak-persistent-storage"
+          }
+        }
+      }
+    }
+
+    volume_claim_template {
+      metadata {
+        name = "keycloak-persistent-storage"
+      }
+      spec {
+        access_modes = ["ReadWriteOnce"]
+        resources {
+          requests = {
+            storage = "2Gi"
+          }
+        }
+        storage_class_name = "civo-volume"
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "keycloak" {
+  metadata {
+    name      = "keycloak"
+    namespace = var.namespace
+    labels = {
+      app = "keycloak"
+    }
+  }
+
+  spec {
+    selector = {
+      app = "keycloak"
+    }
+
+    port {
+      name        = "http"
+      port        = 80
+      target_port = 8080
+    }
+
+    type = "ClusterIP"
+  }
 }
