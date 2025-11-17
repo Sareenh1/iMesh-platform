986064ff59918da4d4e7db3d8eda5b81de1cdfc4
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 14:27:33 +0530

    Update variables.tf

--- imesh-terraform/modules/dependencies/variables.tf
+++ imesh-terraform/modules/dependencies/variables.tf
@@ -2,3 +2,12 @@ variable "namespace" {
   description = "Namespace for dependencies"
   type        = string
 }
+
+variable "keycloak_admin" {
+  description = "Keycloak admin credentials"
+  type = object({
+    username = string
+    password = string
+  })
+  sensitive = true
+}
