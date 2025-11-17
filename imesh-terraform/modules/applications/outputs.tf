470ccdff0b536f5c6a508461b44c1cdd2a20563c
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 14:30:21 +0530

    Update outputs.tf

--- imesh-terraform/modules/applications/outputs.tf
+++ imesh-terraform/modules/applications/outputs.tf
@@ -1,33 +1,3 @@
-output "ui_service_url" {
-  description = "UI service URL"
-  value       = "http://ui.${var.namespace}.svc.cluster.local"
-}
-
-output "graphql_backend_service_url" {
-  description = "GraphQL backend service URL"
-  value       = "http://graphql-backend.${var.namespace}.svc.cluster.local"
-}
-
-output "cluster_backend_service_url" {
-  description = "Cluster backend service URL"
-  value       = "http://cluster-backend.${var.namespace}.svc.cluster.local"
-}
-
-output "graphql_endpoint" {
-  description = "GraphQL endpoint URL"
-  value       = "http://graphql-backend.${var.namespace}.svc.cluster.local/graphql"
-}
-
-output "ui_external_url" {
-  description = "UI external URL"
-  value       = "https://app.${var.domain}"
-}
-
-output "services_external_url" {
-  description = "Services external URL"
-  value       = "https://services.${var.domain}"
-}
-
 output "deployed_services" {
   description = "List of deployed services"
   value = [
@@ -37,7 +7,17 @@ output "deployed_services" {
   ]
 }
 
-output "docker_registry_secret" {
-  description = "Name of the Docker registry secret"
-  value       = kubernetes_secret.docker_registry.metadata[0].name
+output "ui_service_url" {
+  description = "UI service URL"
+  value       = "http://ui.${var.namespace}.svc.cluster.local"
+}
+
+output "graphql_service_url" {
+  description = "GraphQL service URL"
+  value       = "http://graphql-backend.${var.namespace}.svc.cluster.local"
+}
+
+output "cluster_backend_service_url" {
+  description = "Cluster backend service URL"
+  value       = "http://cluster-backend.${var.namespace}.svc.cluster.local"
 }
