fc82fb0160c98a065c364348c9bd710db4851c6d
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 14:28:42 +0530

    Update variables.tf

--- imesh-terraform/modules/keycloak/variables.tf
+++ imesh-terraform/modules/keycloak/variables.tf
@@ -3,6 +3,11 @@ variable "namespace" {
   type        = string
 }
 
+variable "domain" {
+  description = "Base domain"
+  type        = string
+}
+
 variable "admin_user" {
   description = "Keycloak admin username"
   type        = string
@@ -12,22 +17,4 @@ variable "admin_pass" {
   description = "Keycloak admin password"
   type        = string
   sensitive   = true
-}
-
-variable "realm_config" {
-  description = "Keycloak realm configuration"
-  type = object({
-    name         = string
-    display_name = string
-  })
-}
-
-variable "clients" {
-  description = "Keycloak clients configuration"
-  type = map(object({
-    client_id     = string
-    public_client = bool
-    redirect_uris = list(string)
-    web_origins   = list(string)
-  }))
 }
