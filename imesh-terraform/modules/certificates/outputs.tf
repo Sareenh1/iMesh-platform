0c864afbaef674fd9fd024247999dd44800055e3
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 14:09:59 +0530

    Update outputs.tf

--- imesh-terraform/modules/certificates/outputs.tf
+++ imesh-terraform/modules/certificates/outputs.tf
@@ -4,3 +4,31 @@ output "certificate_secrets" {
     nats_server_cert = kubernetes_secret.nats_server_cert.metadata[0].name
   }
 }
+
+output "ca_certificate" {
+  description = "CA certificate PEM"
+  value       = tls_self_signed_cert.ca.cert_pem
+  sensitive   = true
+}
+
+output "nats_certificate" {
+  description = "NATS server certificate PEM"
+  value       = tls_locally_signed_cert.nats.cert_pem
+  sensitive   = true
+}
+
+output "nats_private_key" {
+  description = "NATS server private key PEM"
+  value       = tls_private_key.nats.private_key_pem
+  sensitive   = true
+}
+
+output "cert_manager_status" {
+  description = "Cert Manager Helm release status"
+  value       = helm_release.cert_manager.status
+}
+
+output "cluster_issuer_name" {
+  description = "Cluster issuer name"
+  value       = "letsencrypt-prod"
+}
