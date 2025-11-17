678dee29c52834f5fabafceab64ff30891f34e06
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 14:29:03 +0530

    Update outputs.tf

--- imesh-terraform/modules/keycloak/outputs.tf
+++ imesh-terraform/modules/keycloak/outputs.tf
@@ -4,31 +4,6 @@ output "graphql_backend_secret" {
   sensitive   = true
 }
 
-output "realm_name" {
-  description = "Name of the created Keycloak realm"
-  value       = var.realm_config.name
-}
-
-output "ui_client_id" {
-  description = "UI client ID"
-  value       = var.clients.ui.client_id
-}
-
-output "graphql_backend_client_id" {
-  description = "GraphQL backend client ID"
-  value       = var.clients.graphql_backend.client_id
-}
-
-output "keycloak_admin_user" {
-  description = "Keycloak admin username"
-  value       = var.admin_user
-}
-
-output "keycloak_service_url" {
-  description = "Keycloak service URL"
-  value       = "http://keycloak.${var.namespace}.svc.cluster.local"
-}
-
 output "configuration_complete" {
   description = "Indicates if Keycloak configuration is complete"
   value       = null_resource.configure_keycloak.id != "" ? "complete" : "pending"
