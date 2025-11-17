d7ec5904536a64764e25a0cd5110120559686ff4
H Sareen Kumar <sareenh10@gmail.com>
2025-11-17 14:07:28 +0530

    Create outputs.tf

---
+++ imesh-terraform/modules/dependencies/outputs.tf
@@ -0,0 +1,39 @@
+output "mongodb_url" {
+  description = "MongoDB connection URL"
+  value       = "mongodb://mongodb.${var.namespace}.svc.cluster.local:27017"
+}
+
+output "redis_url" {
+  description = "Redis connection URL"
+  value       = "redis://redis-master.${var.namespace}.svc.cluster.local:6379"
+}
+
+output "nats_url" {
+  description = "NATS connection URL"
+  value       = "nats://nats.${var.namespace}.svc.cluster.local:4222"
+}
+
+output "keycloak_url" {
+  description = "Keycloak service URL"
+  value       = "http://keycloak.${var.namespace}.svc.cluster.local"
+}
+
+output "keycloak_internal_url" {
+  description = "Keycloak internal service URL"
+  value       = "http://keycloak.${var.namespace}.svc.cluster.local:8080"
+}
+
+output "mongodb_helm_release" {
+  description = "MongoDB Helm release status"
+  value       = helm_release.mongodb.status
+}
+
+output "redis_helm_release" {
+  description = "Redis Helm release status"
+  value       = helm_release.redis.status
+}
+
+output "nats_helm_release" {
+  description = "NATS Helm release status"
+  value       = helm_release.nats.status
+}
